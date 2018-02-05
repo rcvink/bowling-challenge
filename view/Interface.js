b9345dd00f04ccf70ff3395da6ffd7e9440dd462
rcvink <rrvink@gmail.com>
2018-02-05 00:52:56 +0000

    WIP Interface

--- view/Interface.js
+++ view/Interface.js
@@ -1,52 +1,60 @@
 $(document).ready(function() {
+  var game = new Game();
+  var frame = new Frame();
+  game.addFrame(frame);
+
   $('#1').on('click', function() {
-    var regularRoll = new RegularRoll(1);
-    console.log(regularRoll);
+    var roll = new Roll(1);
+    addRollToFrame(roll);
   });
 
   $('#2').on('click', function() {
-    var regularRoll = new RegularRoll(2);
-    console.log(regularRoll);
+    var roll = new Roll(2);
+    addRollToFrame(roll);
   });
 
   $('#3').on('click', function() {
-    var regularRoll = new RegularRoll(3);
-    console.log(regularRoll);
+    var roll = new Roll(3);
+    addRollToFrame(roll);
   });
 
   $('#4').on('click', function() {
-    var regularRoll = new RegularRoll(4);
-    console.log(regularRoll);
+    var roll = new Roll(4);
+    addRollToFrame(roll);
   });
 
   $('#5').on('click', function() {
-    var regularRoll = new RegularRoll(5);
-    console.log(regularRoll);
+    var roll = new Roll(5);
+    addRollToFrame(roll);
   });
 
-  $('#6').on('click', function() {
-    var regularRoll = new RegularRoll(6);
-    console.log(regularRoll);
+  $('#6').on('c2lick', function() {
+    var roll = new Roll(6);
+    addRollToFrame(roll);
   });
 
   $('#7').on('click', function() {
-    var regularRoll = new RegularRoll(7);
-    console.log(regularRoll);
+    var roll = new Roll(7);
+    addRollToFrame(roll);
   });
 
   $('#8').on('click', function() {
-    var regularRoll = new RegularRoll(8);
-    console.log(regularRoll);
+    var roll = new Roll(8);
+    addRollToFrame(roll);
   });
 
   $('#9').on('click', function() {
-    var regularRoll = new RegularRoll(9);
-    console.log(regularRoll);
+    var roll = new Roll(9);
+    addRollToFrame(roll);
   });
 
   $('#10').on('click', function() {
-    var regularRoll = new RegularRoll(10);
-    console.log(regularRoll);
+    var roll = new Roll(10);
+    addRollToFrame(roll);
   });
+
+  function addRollToFrame(roll) {
+    game.currentFrame().addRoll(roll);
+  };
 
 });
